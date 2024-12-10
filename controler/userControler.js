64892b873007d3c4e65b8b91830410528a609a68
numa <emmanuel.saadisarrault@gmail.com>
2024-12-10 11:55:46 +0100

    first commit

--- controler/userControler.js
+++ controler/userControler.js
@@ -2,6 +2,8 @@ const loginView = require('../view/loginView');
 const db = require('../db/db');
 const User = require('../modele/User');
 const registerView = require('../view/registerView');
+const bcrypt = require('bcrypt');
+
 
 // fonction qui affiche le formulaire d'inscription
 function showRegister(req, res){
@@ -15,17 +17,21 @@ function showLogin(req, res) {
 
 // fonction qui lance le traitement des données du formulaire d'enregistrement
 function traitRegister(req, res) {
+    const saltRounds = 10;
     const {username, password} = req.body;
-    const newUser = new User(username, password);
-    const query = 'INSERT INTO users (username, password) VALUES (?,?)';
-    db.run(query, [newUser.username, newUser.password], function(err){
-        if(err){
-            console.error('echec enregistrement', err.message);
-            res.send('error');
-        } else {
-            console.log('user success', newUser);
-            res.send('register success');
-        }
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+        const newUser = new User(username, hashedPassword);
+        const query = 'INSERT INTO users (username, password) VALUES (?,?)';
+        db.run(query, [newUser.username, newUser.password], 
+            function(err){
+            if(err){
+                console.error('echec enregistrement', err.message);
+                res.send('error');
+            } else {
+                console.log('user success', newUser);
+                res.send('register success');
+            }
+    })
     });
 }
 
@@ -33,14 +39,25 @@ function traitRegister(req, res) {
 function traitLogin(req, res) {
     const {username, password} = req.body;
     const newUser= new User(username, password);
-    const query = 'SELECT * FROM users WHERE username = ? AND password = ?'
-    db.all(query, [username, password], function(err){
+    const query = 'SELECT * FROM users WHERE username = ?'
+    db.get(query, [newUser.username], function(err, row){
         if(err){
             console.error('echec connexion', err.message);
             res.send('error');
-        } else {
-            console.log('user success', newUser);
-            res.send('connexion success');
+        }
+        if(row){
+            bcrypt.compare(password, row.password, (err, result) => {
+                if(err){
+                    console.log("mdp non correspondant");
+                    res.send('echec comparaison');
+                } else if(result){
+                    console.log(newUser, 'Connecté');
+                    res.send('bienvenue');
+                }else {
+                    console.log('mauvais mdp');
+                    res.send('mot de passe éronné');
+                }
+            } );
         }
     });
 }
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const {showLogin, showRegister, traitRegister, traitLogin} = require('./controler/userControler');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// créer une session
+app.use(session({
+    secret: 'manuKey',  // Une clé secrète utilisée pour signer les cookies de session
+    resave: false,                // Ne pas sauver la session si elle n'a pas été modifiée
+    saveUninitialized: true,      // Sauvegarder une session non initialisée (si elle est vide)
+    cookie: { secure: false }     // Ne nécessite pas une connexion HTTPS pour l'exemple (en production, utilisez `secure: true`)
+  }));
 
 // Middleware pour analyser le JSON
 app.use(express.json());
@@ -10,15 +19,16 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 // écoute le port 3000 -> accueil
-app.listen(3000, (res,req) => {console.log('coucou')});
+app.listen(3000, (req, res) => {console.log('coucou')});
 
 // envoie l'affichage du formulaire de connexion via la méthodie GET
-app.get('/login', (res, req) => {showLogin(res, req);});
+app.get('/login', (req, res) => {showLogin(req, res);});
 
 // envoie l'affichage du formulaire d'enregistrement via la méthodie GET
-app.get('/register', (res, req) => {showRegister(res, req);});
+app.get('/register', (req, res) => {showRegister(req, res);});
 
 // envoie le traitement des données de formulaire d'enregistrement
-app.post('/register', (res, req) => {traitRegister(res, req);});
+app.post('/register', (req, res) => {traitRegister(req, res);});
 
-app.post('/login', (res, req) => {traitLogin(res, req);});
+// envoie le traitement des données de formulaire de connexion
+app.post('/login', (req, res) => {traitLogin(req, res);});
